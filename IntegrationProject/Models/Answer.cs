ca9f08e2022e7e723753a34c58639567a6b9e44c
alynielson <nielsonaly@gmail.com>
2018-09-10 11:05:32 -0500

    change value types on ANswers table

--- IntegrationProject/Models/Answer.cs
+++ IntegrationProject/Models/Answer.cs
@@ -13,12 +13,12 @@ namespace IntegrationProject.Models
 
         public double Price { get; set; }
 
-        public List<int> Activities { get; set; }
-        public List<int> Drinks { get; set; }
-        public List<int> Foods { get; set; }
+        public string Activities { get; set; }
+        public string Drinks { get; set; }
+        public string Foods { get; set; }
         public double SitDown { get; set; }
         public double TimeOfDay { get; set; }
-        public List<int> Music { get; set; }
+        public string Music { get; set; }
         public double DressCode { get; set; }
         public double Age { get; set; }
         public int Master { get; set; }
