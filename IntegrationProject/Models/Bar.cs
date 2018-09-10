c2b109c928063c9b08f50fa6b1648873ab8cd00c
Marco-Malacara <marco.malacara7@gmail.com>
2018-09-10 09:44:53 -0500

    added bar model

--- IntegrationProject/Models/Bar.cs
+++ IntegrationProject/Models/Bar.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace IntegrationProject.Models
 {
     public class Bar
     {
+        [Key]
+        public int Id { get; set; }
+        public string YelpId { get; set; }
+        [ForeignKey("AnswerId")]
+        public int AnswerId { get; set; }
+        public Answer Answer { get; set; }
     }
 }
