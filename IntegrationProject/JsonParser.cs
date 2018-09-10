8f0e64d3df29b3c7aac83a11b6ca854ed6ad0109
alynielson <nielsonaly@gmail.com>
2018-09-10 07:52:07 -0500

    parsing json file from yelp search request

---
+++ IntegrationProject/JsonParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace IntegrationProject
+{
+    public static class JsonParser
+    {
+        public static List<string> ParseYelpSearch()
+        {
+            string url = $"https://api.yelp.com/v3/businesses/search?term=bars&latitude=43.031605&longitude=-87.909850&radius=400";
+            WebResponse response = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Headers["Authorization"] = Credentials.yelpApiKey;
+                request.Method = "GET";
+                response = request.GetResponse();
+                if (response != null)
+                {
+                    string responseString = null;
+                    Stream stream = response.GetResponseStream();
+                    StreamReader streamReader = new StreamReader(stream);
+                    responseString = streamReader.ReadToEnd();
+                    SearchResult searchResults = JsonConvert.DeserializeObject<SearchResult>(responseString);
+                    return searchResults.businesses.Select(b => b.name).ToList();
+                }
+                else
+                {
+                    throw new Exception("Unable to get response");
+                }
+
+            }
+            catch
+            {
+                throw new Exception("Request url not valid");
+            }
+        }
+    }
+}
+}
---
+++ IntegrationProject/YelpSearchResults.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationProject
+{
+    public class SearchResult
+    {
+        public int total { get; set; }
+        public Region region { get; set; }
+
+        public Business[] businesses { get; set; }
+    }
+
+    public class ViewModel
+    {
+        public List<string> barNames { get; set; }
+    }
+
+    public class Business
+    {
+        public string id { get; set; }
+        public string alias { get; set; }
+        public string name { get; set; }
+        public string image_url { get; set; }
+        public string url { get; set; }
+        public int review_count { get; set; }
+        public Categories[] categories { get; set; }
+        public double rating { get; set; }
+        public Coordinates coordinates { get; set; }
+        public string[] transactions { get; set; }
+        public Location location { get; set; }
+        public string phone { get; set; }
+        public string display_phone { get; set; }
+        public decimal distance { get; set; }
+
+    }
+
+    public class Location
+    {
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string address3 { get; set; }
+        public string city { get; set; }
+        public string zip_code { get; set; }
+        public string country { get; set; }
+        public string state { get; set; }
+        public string[] display_address { get; set; }
+    }
+    public class Region
+    {
+        public Coordinates center { get; set; }
+    }
+
+    public class Coordinates
+    {
+        public decimal longitude { get; set; }
+        public decimal latitude { get; set; }
+    }
+
+    public class Categories
+    {
+        public string alias { get; set; }
+        public string title { get; set; }
+    }
+
+
+}
