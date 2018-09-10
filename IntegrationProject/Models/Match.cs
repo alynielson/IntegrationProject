5ec623790bd648d6aad1a7357d6824f3bd3fbe34
Trenton Daniels <trentondaniels95@gmail.com>
2018-09-10 10:02:34 -0500

    created match table

---
+++ IntegrationProject/Models/Match.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationProject.Models
+{
+    public class Match
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Score { get; set; }
+        [ForeignKey("Bar")]
+        public int BarId { get; set; }
+        public Bar Bar { get; set; }
+        [ForeignKey("Member")]
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+        
+    }
+}
