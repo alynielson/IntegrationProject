d797e701e801be3085687c2a5395dad17b6230bd
alynielson <nielsonaly@gmail.com>
2018-09-11 13:47:58 -0500

    backbone of survey analyzer class

---
+++ IntegrationProject/SurveyAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using IntegrationProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationProject
+{
+    public static class SurveyAnalyzer
+    {
+        public static void GetSurveyResults()
+        {
+            //ApplicationDbContext db = new ApplicationDbContext();
+            int heaviestWeightQuestion = GetHeaviestWeightedQuestion();
+            AssignQuestionWeights(heaviestWeightQuestion);
+        }
+
+        private static int GetHeaviestWeightedQuestion()
+        {
+            return 3;
+        }
+
+        private static void AssignQuestionWeights(int heaviestWeightQuestion)
+        {
+
+        }
+
+       
+    }
+}
