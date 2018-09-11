fcd80fb7e8f6a9c0bb1fa9f78977b9f37bf23272
alynielson <nielsonaly@gmail.com>
2018-09-11 14:53:45 -0500

    edits to create member

--- IntegrationProject/Survey.cs
+++ IntegrationProject/Survey.cs
@@ -8,15 +8,15 @@ namespace IntegrationProject
 {
     public static class Survey
     {
-        public static Dictionary<string, bool> PEOPLE { get; } = new Dictionary<string, bool>()
+        public static Dictionary<string, double> PEOPLE { get; } = new Dictionary<string, double>()
         {
-            {"Yes", true },
-            {"No", false }
+            {"Yes", 1},
+            {"No", 0 }
         };
-        public static Dictionary<string, bool> SITDOWN { get; } = new Dictionary<string, bool>()
+        public static Dictionary<string, double> SITDOWN { get; } = new Dictionary<string, double>()
         {
-            {"Yes", true },
-            {"No", false }
+            {"Yes", 1 },
+            {"No", 0 }
         };
         public static Dictionary<string, double> PRICE { get; } = new Dictionary<string, double>()
         {
