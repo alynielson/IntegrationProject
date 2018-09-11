354b7ea8f1fb6391f775d9aa01d6deae730985df
Marco-Malacara <marco.malacara7@gmail.com>
2018-09-11 14:32:46 -0500

    made cards of restraunts and added button for going to full detail

--- IntegrationProject/Startup.cs
+++ IntegrationProject/Startup.cs
@@ -18,22 +18,6 @@ namespace IntegrationProject
 {
     public class Startup
     {
-        //private async Task CreateRoles(IServiceProvider serviceProvider)
-        //{
-        //    var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        //    var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-        //    string[] roleNames = { "Admin", "Member" };
-        //    IdentityResult roleResult;
-
-        //    foreach (var roleName in roleNames)
-        //    {
-        //        var roleExist = await RoleManager.RoleExistsAsync(roleName);
-        //        if (!roleExist)
-        //        {
-        //            roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
-        //        }
-        //    }
-        //}
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
