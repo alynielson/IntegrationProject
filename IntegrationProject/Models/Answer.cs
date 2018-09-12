5f153f740c3b1207e2636c9824c92f5ab2f33655
Trenton Daniels <trentondaniels95@gmail.com>
2018-09-12 10:35:14 -0500

    fixed answer.cs to account for checkbox lists

--- IntegrationProject/Models/Answer.cs
+++ IntegrationProject/Models/Answer.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-<<<<<<< HEAD
-=======
 using System.ComponentModel.DataAnnotations.Schema;
->>>>>>> d332ea2c7e1288159398270e81b6281aecd043c3
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,17 +11,14 @@ namespace IntegrationProject.Models
     {
         [Key]
         public int Id { get; set; }
-        
         public double People { get; set; }
-
         public double Price { get; set; }
-
-        public string Activities { get; set; }
+        public List<Activity> Activities { get; set; }
         public List<Drink> Drinks { get; set; }
-        public string Foods { get; set; }
+        public List<Food> Foods { get; set; }
         public double SitDown { get; set; }
         public double TimeOfDay { get; set; }
-        public string Music { get; set; }
+        public List<Music> Musics { get; set; }
         public double DressCode { get; set; }
         public double Age { get; set; }
         public int Master { get; set; }
@@ -32,6 +26,28 @@ namespace IntegrationProject.Models
     }
 
     public class Drink
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public bool Checked { get; set; }
+    }
+    public class Food
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public bool Checked { get; set; }
+    }
+
+    public class Activity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public bool Checked { get; set; }
+    }
+    public class Music
     {
         [Key]
         public int Id { get; set; }
