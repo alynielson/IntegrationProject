5adde4411c70935ba0b217be1e392d53d9d28eea
alynielson <nielsonaly@gmail.com>
2018-09-11 19:07:01 -0500

    get question answers from db for survey analyzer

--- IntegrationProject/SurveyAnalyzer.cs
+++ IntegrationProject/SurveyAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using IntegrationProject.Data;
+using IntegrationProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +9,41 @@ namespace IntegrationProject
 {
     public static class SurveyAnalyzer
     {
-        public static void GetSurveyResults()
+        public static void GetSurveyResultsBar(ApplicationDbContext context, Bar bar)
         {
-            //ApplicationDbContext db = new ApplicationDbContext();
-            int heaviestWeightQuestion = GetHeaviestWeightedQuestion();
-            AssignQuestionWeights(heaviestWeightQuestion);
+            int barAnswers = GetBarAnswersId(bar);
+            Answer answer = GetAnswersFromDb(context, barAnswers);
+
         }
 
-        private static int GetHeaviestWeightedQuestion()
+        public static void GetSurveyResultsMember(ApplicationDbContext context, Member member)
         {
-            return 3;
+            int memberAnswers = GetMemberAnswersId(member);
+            Answer answer = GetAnswersFromDb(context, memberAnswers);
+            int heaviestWeightQuestion = GetHeaviestWeightedQuestion(answer);
+            AssignQuestionWeights(heaviestWeightQuestion);
+        }
+        private static int GetBarAnswersId(Bar bar)
+        {
+            int barAnswers = bar.AnswerId;
+            return barAnswers;
+        }
+
+        private static int GetMemberAnswersId(Member member)
+        {
+            int memberAnswers = member.AnswerId;
+            return memberAnswers;
+        }
+        private static Answer GetAnswersFromDb(ApplicationDbContext context, int AnswersId)
+        {
+            Answer answers = context.Answers.Find(AnswersId);
+            return answers;
+        }
+
+        private static int GetHeaviestWeightedQuestion(Answer answers)
+        {
+            int heaviestWeightQuestion = answers.Master;
+            return heaviestWeightQuestion;
         }
 
         private static void AssignQuestionWeights(int heaviestWeightQuestion)
