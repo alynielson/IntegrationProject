27ebc5188ea66d05e590ddd46be1bcaef25d24c3
Marco-Malacara <marco.malacara7@gmail.com>
2018-09-12 19:11:07 -0500

    attempting to add yelp reviews through api to details page

--- IntegrationProject/YelpReviews.cs
+++ IntegrationProject/YelpReviews.cs
@@ -16,6 +16,7 @@ namespace IntegrationProject
     {
         public string id { get; set; }
         public string url { get; set; }
+        public string text { get; set; }
         public int rating { get; set; }
         public string time_created { get; set; }
         public User user { get; set; }
