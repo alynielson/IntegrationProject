e342c9757b1a417dea94bd82d1a7a89018e35c02
Marco-Malacara <marco.malacara7@gmail.com>
2018-09-17 09:26:20 -0500

    imgur api text to method fix

--- IntegrationProject/Controllers/BarController.cs
+++ IntegrationProject/Controllers/BarController.cs
@@ -214,14 +214,14 @@ namespace IntegrationProject.Controllers
         {
             return _context.Bars.Any(e => e.Id == id);
         }
-        private async Task<string> UploadImageAsync(string imageUrl)
+        private async Task<string> UploadImageAsync(string Url)
         {
             byte[] response;
             using (var client = new WebClient())
             {
                 string clientID = Credentials.ImgurApi;
                 client.Headers.Add("Authorization", "Client-ID " + clientID);
-                var values = new NameValueCollection { { "image",imageUrl } };
+                var values = new NameValueCollection { { "image",Url } };
                 response = await client.UploadValuesTaskAsync("https://api.imgur.com/3/upload", values);
             }
 
